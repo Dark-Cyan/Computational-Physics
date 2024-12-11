6b1b3f62e8e0a824215c6455846f3a04a4a9989d
duncansciubba <dmsciubba@gmail.com>
2024-12-11 11:59:54 -0500

    Started Work on Suika Game

--- Unit 4/Midterm/physics.py
+++ Unit 4/Midterm/physics.py
@@ -1,18 +1,42 @@
-from vectors import*
+from stuff import*
 
 g = Vec(0,-9.8,0)
 t=0
-dt=0.01
+dt=0.1
 
 def weight(a):
     return a.m*g
 
-def forces(a):
-    a.force=weight(a)
+def netForce(list):
+    forces = []
+    for i in range(len(list)):
+        if list[i].anchor:
+            forces.append(0)
+            continue
+        magnitude = list[i].grav
+        for j in range(len(list)):
+            if j == i:
+                continue
+            r = list[j].pos - list[i].pos
+            if r == 0:
+                magnitude = magnitude
+            elif abs(r) <= list[i].r + list[j].r:
+                A = ((list[j].pos-list[i].pos).norm())
+                magnitude = magnitude + 6*(A * A.dot(list[i].pos))
+        forces.append(magnitude)
+    return forces
 
-def move(a, reps):
+def move(list, reps):
+    global t
+    global run
+
     for i in range(reps):
-        forces()
-        a.acc=a.force/a.m
-        a.vel+=a.acc*dt
-        a.pos+=a.vel*dt
+        for j in range(reps):
+            forces = netForce(list)
+            t+=dt
+            for i in range(len(list)):
+                acc=forces[i]/list[i].m
+                list[i].vel+=acc*dt
+                if list[i].pos.y <= 0:
+                    continue
+                list[i].pos+=list[i].vel*dt
--- Unit 4/Midterm/stuff.py
+++ Unit 4/Midterm/stuff.py
@@ -1,12 +1,16 @@
 from vectors import*
 
 class stuff:
-    def __init__(self, mass, position, gravity, anchor):
+    def __init__(self, mass, radius, position, gravity, anchor):
         self.m = mass
+        self.r = radius
         self.pos = position
         self.vel = Vec(0,0,0)
         self.acc = Vec(0,0,0)
         self.force = Vec(0,0,0)
         self.grav = gravity
         self.anchor = anchor
+        fruits.append(self)
+
+fruits = []
 
