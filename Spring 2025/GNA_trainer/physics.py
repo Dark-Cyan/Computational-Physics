8897a9027ce4278a101ab2a6ce06cb30b327000d
duncansciubba <dmsciubba@gmail.com>
2025-01-14 10:43:20 -0500

    Idk

--- Spring 2025/GNA_trainer/physics.py
+++ Spring 2025/GNA_trainer/physics.py
@@ -19,4 +19,10 @@ def drag(a):
     return -0.5*p*a.C*math.pi*(a.r**2)*a.vel*a.vel.mag() #1/2CApv^2
 
 def magnus(a):
-    return a.mC*a.w
+    return a.mC*a.w.Cross(a.vel)
+
+def forces(a):
+    return weight(a) + drag(a) + magnus(a)
+
+tester = pop.Ball(45, 42, 100, 2)
+print(magnus(tester))
