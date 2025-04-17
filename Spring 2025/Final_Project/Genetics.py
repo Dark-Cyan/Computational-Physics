e1bca6682b3da814e8603d9b625998ad970a51a3
duncansciubba <dmsciubba@gmail.com>
2025-04-17 12:59:22 -0400

    Started Work on Final Project

---
+++ Spring 2025/Final_Project/Genetics.py
@@ -0,0 +1,7 @@
+class Chromosome:
+    def __init__(self, list):
+        Genes = list
+
+class Gene:
+    def __init__(self):
+        pass
---
+++ Spring 2025/Final_Project/Munchkins.py
@@ -0,0 +1,9 @@
+class Munchkin:
+    def __init__(self, paternalSet, maternalSet):
+        self.paternalSet = paternalSet
+        self.maternalSet = maternalSet
+
+    
+[]
+[]
+Tristan = Munchkin(['R', 'B'],['r', 'b'])
